6695357dfdd65e4b711d1e6b24b5649f2df3f290
Jagadeesh <jagadeesh.javali@gmail.com>
2020-10-05 17:43:49 +0530

    Added Project

---
+++ src/test/java/TestNG/Selenium/MyFirstTest.java
@@ -0,0 +1,22 @@
+package TestNG.Selenium;
+
+import org.testng.annotations.Test;
+
+
+public class MyFirstTest {
+	
+	@Test
+	public void validateLoginforRetail() {
+		
+		System.out.println("LOG:: validateLoginforRetail");
+		
+	}
+	
+	@Test
+	public void validateLoginforBusiness() {
+		
+		System.out.println("LOG:: validateLoginforBusiness");
+		
+	}
+
+}
---
+++ src/test/java/TestNG/Selenium/MySecondTest.java
@@ -0,0 +1,22 @@
+package TestNG.Selenium;
+
+import org.testng.annotations.Test;
+
+
+public class MySecondTest {
+
+	
+	@Test
+	public void validateLogOutinforRetail() {
+		
+		System.out.println("LOG:: validateLogOutinforRetail");
+		
+	}
+	
+	@Test
+	public void validateLogoutforBusiness() {
+		
+		System.out.println("LOG:: validateLogoutforBusiness");
+		
+	}
+}
